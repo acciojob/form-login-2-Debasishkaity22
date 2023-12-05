4082e79246249fd9d7de90806b1761a8d351e866
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-05 20:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let de=document.querySelector("#form-info");
+
+function deba(){
+   let debasish={
+    first:"Firs Name:"+de.firstname.value,
+    last:"Lastst Name:"+de.lastname.value,
+    phone:"PhoneNumber:"+de.phoneno.value,
+    Email:"Email ID:"+de.emailid.value
+   }
+  alert(Object.values(debasish));
+}
